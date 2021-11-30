61d6b401a1eb23340ba3e717d3ffc9dc55eded62
Saori Shigehisa <kumakichi3173@gmail.com>
2021-11-30 10:02:21 -0500

    Update RandomGen.v

--- RandomGen.v
+++ RandomGen.v
@@ -3,7 +3,25 @@
 module RandomGen(Clk, Rst, RandomValue);
 
    input Clk, Rst;
-   output [ ] RandomValue; // you need decide the number of bits here.
+   output [12:20] RandomValue; // you need decide the number of bits here.
    
-   
+   parameter MinValue = 1000;
+   parameter MaxValue = 6000;
+
+   reg [12:0] Value;
+
+   always @(posedge Clk) begin
+      if(Rst == 1) begin
+         Value <= MinValue;
+      end
+      else begin
+         if(Value == MaxValue) begin
+            Value <= MinValue;
+      end
+      else begin
+         Value <= Value + 1;
+      end
+   end
+
 endmodule
+
